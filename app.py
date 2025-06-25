4946b88a083c6d2eb1358116e15a8d6250e95976
Emprendisis <emprendisis@gmail.com>
2025-06-25 17:21:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def montecarlo_simulator(frequency_type, frequency_params, severity_type, severi
     return df, stats
 
 # Streamlit app
-st.title("Simulador Monte Carlo con Análisis de Riesgos Mejorado")
+st.title("Simulador Montecarlo con Análisis de Riesgos Mejorado")
 
 # Inputs Frecuencia
 st.sidebar.header("Frecuencia")
